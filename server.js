a3bdfb33e658a25517f5bf0584b69ef975d03016
rohitdrake <pirnagrasishwa@gmail.com>
2017-04-15 00:34:04 +0530

    debugged port bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const MongoClient=require('mongodb').MongoClient;
 
 let app=express();
-app.set('port', (process.env.port||3000));
+app.set('port', (process.env.PORT||3000));
 app.listen(app.get('port'), ()=>{
   console.log('server is listening at '+app.get('port'));
 });
